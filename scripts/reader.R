db58c5f4a393dd1c984e2e581895882f5a8d4b26
ÁlvaroPL <aaparede@uc.cl>
2021-10-18 21:39:09 -0300

    typo fix

--- scripts/reader.R
+++ scripts/reader.R
@@ -7,7 +7,7 @@ source('scripts/functions.R')
 e2017_1v = prep_table(read_excel('data/07-Elecciones Presidencial, Parlamentarias y de Cores 2017/Resultados_Mesa_PRESIDENCIAL_Tricel_1v_DEF.xlsx'))
 e2017_2v = prep_table(read_excel('data/07-Elecciones Presidencial, Parlamentarias y de Cores 2017/Resultados_Mesa_PRESIDENCIAL_Tricel_2v_DEF.xlsx'))
 e2018 = prep_table(read_excel('data/08-Plebiscito Nacional 2020/Resultados Plebiscito Constitucion Politica 2020_DEF.xlsx'))
-e2020_cp = prep_table(read_excel('data08-Plebiscito Nacional 2020/Resultados Plebiscito Constitucion Politica 2020_DEF.xlsx'))
+e2020_cp = prep_table(read_excel('data/08-Plebiscito Nacional 2020/Resultados Plebiscito Constitucion Politica 2020_DEF.xlsx'))
 
 elecciones_lista <- list(e2017_1v, e2018, e2020_cp)
 
