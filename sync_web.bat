d03057b6deb0b71b8022cf3f25ca5aac4fe46c54
ÁlvaroPL <aaparede@uc.cl>
2021-11-20 18:21:15 -0300

    sync data con el servidor y limpiar caché

--- sync_web.bat
+++ sync_web.bat
@@ -1,2 +1,4 @@
 aws s3 sync website/_book/. s3://analisislegislativo --profile myaws --acl public-read
-aws cloudfront create-invalidation --distribution-id E2IGLSZ2P7FESQ --paths "/*"
+aws cloudfront create-invalidation --distribution-id E2IGLSZ2P7FESQ --paths / --profile myaws
+
+echo "Deployment complete"
