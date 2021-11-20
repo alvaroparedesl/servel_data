1d5693e120228ac658837d83131efd659929cf3c
ÁlvaroPL <aaparede@uc.cl>
2021-11-20 18:22:58 -0300

    sync data fix2

--- sync_web.bat
+++ sync_web.bat
@@ -2,3 +2,4 @@ aws s3 sync website/_book/. s3://analisislegislativo --profile myaws --acl publi
 aws cloudfront create-invalidation --distribution-id E2IGLSZ2P7FESQ --paths / --profile myaws
 
 echo "Deployment complete"
+pause
